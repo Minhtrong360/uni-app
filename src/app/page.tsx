e38a6bed76b7774b712aee9bc95638f92809d57b
TakegawaPham <35416129+TakegawaPham@users.noreply.github.com>
2024-09-30 16:26:31 +0700

    Mon 30 Sep 16.16

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
-  console.log("Trick");
+  console.log("Tricks");
 
   return (
     <div className="relative mx-auto -mt-[64px] bg-background">
